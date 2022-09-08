ca43610fd4c4297e124e965d1f33921184b47afc
Lisa Wagner <lisawagner.online@gmail.com>
2022-09-08 12:35:04 -0700

    chore: clean up boilerplate, add tailwind & config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // enable image optimization for api hosted image sources
+  images: {
+    domains: ["image.tmdb.org"]
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+{/* <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link> */}
+
+// font-family: 'Open Sans', sans-serif;
+
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html>
+    <Head>
+      <title>MovieDeets</title>
+      <meta name="description" content="MovieDeets, an app search movie info and to look up new movies to watch. Made with Nextjs, Tailwind, Typescript and The Movie Database API." />
+      <link rel="icon" href="/favicon.ico" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel='stylesheet' />
+    </Head>
+    <body className='font-opensans'>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
