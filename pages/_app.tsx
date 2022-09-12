6f643a5a95a671dd1e7cb91e41e0680e210e5e86
Lisa Wagner <lisawagner.online@gmail.com>
2022-09-11 19:18:18 -0700

    chore: test @tanstack/react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,21 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useState } from 'react'
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 // next imports
 import type { AppProps } from 'next/app'
 // styles
 import '../styles/globals.css'
 
-const client = new QueryClient()
+// const client = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
   return (
     <>
-      <QueryClientProvider client={client}>
+      {/* <QueryClientProvider client={client}> */}
+      <QueryClientProvider client={queryClient}>
+        {/* <Hydrate state={pageProps.dehydratedState}> */}
           <Component {...pageProps} />
+        {/* </Hydrate> */}
       </QueryClientProvider>
     </>
   )
