8a072fed4b5a195c7a0fe6a8d245bf9440430be7
Lisa Wagner <lisawagner.online@gmail.com>
2022-09-13 10:58:58 -0700

    chore: add API endpoints to config

--- config.ts
+++ config.ts
@@ -3,16 +3,30 @@
 
 const API_URL: string = 'https://api.themoviedb.org/3/';
 const API_KEY: string | undefined = process.env.API_KEY;
-const NP_API_KEY: string | undefined = process.env.NEXT_PUBLIC_API_KEY
 
+// Base Urls
 const SEARCH_BASE_URL: string = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL: string = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
 
+const TRENDING_BASE_URL: string = `${API_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`
+// const NETFLIX_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`
+const TOP_RATED_BASE_URL: string = `${API_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`
+
+const ADVENTURE_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=12`
+const ACTION_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`
+const COMEDY_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`
+const FANTASY_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=14`
+// const HORROR_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`
+const SCIFI_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=878`
+const THRILLER_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=53`
+// const ROMANCE_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`
+// const DOCUMENTARY_BASE_URL: string = `${API_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`
+
 // For single movie
 const movieUrl = (id?: string) => `${API_URL}movie/${id}?api_key=${API_KEY}`;
-// const movieUrl = (id?: string) => `${API_URL}movie/${id}?api_key=${NP_API_KEY}`;
 const creditsUrl = (id?: string) => `${API_URL}movie/${id}/credits?api_key=${API_KEY}`;
 
+// Images
 const IMAGE_BASE_URL: string = 'http://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE: string = 'w1280';
@@ -28,5 +42,11 @@ export {
   BACKDROP_SIZE,
   POSTER_SIZE,
   movieUrl,
-  creditsUrl
+  creditsUrl,
+  ADVENTURE_BASE_URL,
+  ACTION_BASE_URL,
+  COMEDY_BASE_URL,
+  FANTASY_BASE_URL,
+  SCIFI_BASE_URL,
+  THRILLER_BASE_URL
 };
