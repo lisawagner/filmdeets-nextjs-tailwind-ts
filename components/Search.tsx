549781295e4bf6698ad0521ff3dc19bc94f30c44
Lisa Wagner <lisawagner.online@gmail.com>
2022-09-14 12:41:14 -0700

    feat: add clear button to search input

--- components/Search.tsx
+++ components/Search.tsx
@@ -10,52 +10,49 @@ type Props = {
 const TIME = 3000; // ms
 
 const Search = ({ setQuery }: Props) => {
-  const [text, setText] = useState('');
   const navigate = useRouter();
-  const timer = useRef<NodeJS.Timeout>(null);
   const [inputVal, setInputVal] = useState("")
   const [isFocused, setIsFocused] = useState(false)
-  const targetRef = useRef(null)
-  
-  const showCloseBtn = isFocused
-  
-  const [isVisible, setIsVisible] = useState(false)
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  // const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const value = e.currentTarget.value;
+  const [hasInput, setHasInput] = useState(false)
 
-  //   clearTimeout(timer.current);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputVal(e.target.value)
+    const value = e.currentTarget.value
 
-  //   setText(value);
+    if (value.length !== 0) {
+      setHasInput(true)
+    } else {
+      setHasInput(false)
+    }
+  }
 
-  //   timer.current = setTimeout(() => {
-  //     setQuery(value);
-  //   }, TIME);
-  // };
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
 
     if (inputVal.trim()) {
       setQuery(inputVal.trim())
     } 
-
   };
+  
   const handleInputReset = () => {
+
     setInputVal('')
-    
     navigate.reload()
-    // clearTimeout(timer.current);
-    // timer.current = setTimeout(() => {
-    //   setIsFocused(false)
-    // }, TIME);
+    
+    // if (inputVal.length !== 0) {
+    //   setInputVal('')
+    //   navigate.reload()
+    // } else return
+
   }
 
-  console.log('IsFocused? ', isFocused);
+  const handleFocus = () => {
+    setIsFocused(true)
+  }
 
-  // useEffect (() => {
-
-  //   targetRef.current.value =""
-  // }, [showCloseBtn])
+  const handleBlur = () => {
+    setIsFocused(false)
+  }
   
   return (
     <div className='relative'>
@@ -64,32 +61,40 @@ const Search = ({ setQuery }: Props) => {
           <span className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"><RiSearchLine className="w-5 h-5 text-cyan-400" /></span>
         </label>
         <input
-          // className='h-10 pr-14 md:w-96 rounded-md p-4 text-md border border-cyan-400 bg-transparent text-cyan-400 focus:outline-none focus:border-2 focus:border-solid focus:border-cyan-300'
           className='flex h-10 pr-14 pl-10 text-sm text-cyan-400 bg-transparent rounded-md border border-cyan-400 focus:outline-none focus:border-1 focus:border-solid focus:border-cyan-300'
           type='text'
           placeholder='Search Movie'
-          onFocus={() => setIsFocused(true)}
-          // onBlur={() => setIsFocused(false)}
-          // value={text}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
           value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
+          onChange={handleChange}
           onKeyDown={(e) => e.stopPropagation()}
           onKeyUp={(e) => e.stopPropagation()}
-          onKeyPress={(e) => e.stopPropagation()}
-          // onChange={handleInput}
-          
+          onKeyPress={(e) => e.stopPropagation()} 
         />
-
-  {/* ${isFocused ? `visible` : `invisible`} */}
       </form>
-      <button
-        ref={buttonRef}
-        onClick={handleInputReset}
-        // onFocus={() => setIsFocused(true)}
-        className={`flex absolute inset-y-0 right-0 items-center pr-3 text-cyan-400`}
-      >
-        x
-      </button>
+
+      {isFocused ? 
+        <>
+        <button
+          onClick={handleInputReset}
+          // onFocus={() => setIsFocused(true)}
+          className={`flex absolute inset-y-0 right-0 items-center pr-3 text-cyan-400`}
+        >
+          x
+        </button>
+        </>
+       : (hasInput ? 
+        <>
+          <button
+          onClick={handleInputReset}
+          // onFocus={() => setIsFocused(true)}
+          className={`flex absolute inset-y-0 right-0 items-center pr-3 text-cyan-400`}
+        >
+          x
+        </button>
+        </> : null) } 
+
     </div>
   );
 };
