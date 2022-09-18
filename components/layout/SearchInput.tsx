c548f90938b20409c5b7fb56247add7580ab4deb
Lisa Wagner <lisawagner.online@gmail.com>
2022-09-18 08:07:25 -0700

    chore: testing search input

--- components/layout/SearchInput.tsx
+++ components/layout/SearchInput.tsx
@@ -3,36 +3,29 @@ import { RiSearchLine } from 'react-icons/ri'
 import { useRouter } from "next/router";
 // components & hooks
 import SearchReset from './SearchReset'
-import useStorage from '../../utils/useStorage'
+
 
 const SearchInput = () => {
   const navigate = useRouter();
   const [inputVal, setInputVal] = useState("")
   const [isFocused, setIsFocused] = useState(false)
   const [hasInput, setHasInput] = useState(false)
+  
+  const [route, setRoute] = useState<string | null>(null)
+
   const [newQueryParams, setNewQueryParams] = useState({})
-  const [initialParams, setInitialParams] = useState({})
+  const [initialParams, setInitialParams] = useState({ name: "" })
 
   const { pathname } = useRouter()
   
-  
-  console.log("URLPARAMS: ", navigate.query.name)
-  console.log('INPUTVAL: ', inputVal);
-  
-  
-  // const initialParam = navigate.query.name
-  // console.log(initialParam);
-  
-  // const movieRef = useRef(pathname)
-  
-  // console.log("URLPARAMS: ", navigate.query.name);
-  // setInitialParams(navigate.query)
-  // console.log("PARAMS STATE: ", initialParams);
+  // console.log("URLPARAMS: ", navigate.query.name)
 
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputVal(e.target.value)
+
     const value = e.currentTarget.value
+    setRoute(value)
 
     if (value.length !== 0) {
       setHasInput(true)
@@ -40,37 +33,25 @@ const SearchInput = () => {
       setHasInput(false)
     }
   }
+
+
   // TODO: Fix - Search page doesn't allow a new search? input val check?
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-    
-    let initialVal = inputVal
-    console.log("INITIALVAL: ", initialVal);
-    
-    // setInitialParams({
-    //     pathname: '/movies/search/',
-    //     query: { name: `${initialVal}` }
-    //   })
-    
-    // console.log("INIIAL: " + initialParams);
-    // console.log("QUERY: ", navigate.query);
-
-    if (inputVal === navigate.pathname) {
-      console.log('Movies equal!!!!');
-    } else {
-      console.log('Do something here');
-      
-    }
-
-    // navigate.push(newQueryParams)
-
+     
+    // navigate.push({
+    //   pathname: '/movies/search/',
+    //   query: { name: `${route}` }
+    // })
+  
     navigate.push({
       pathname: '/movies/search/',
       query: { name: `${inputVal}` }
     })
 
     setInputVal('')
-    navigate.replace('/movies/search', undefined, { shallow: true })
+    // navigate.replace('/movies/search', undefined, { shallow: true })
+  
     // if (inputVal.trim()) {
     //   setNewQuery(inputVal.trim())
     //   // route to search results page
@@ -87,21 +68,12 @@ const SearchInput = () => {
     navigate.push('/')
   }
 
-  const handleFocus = () => {
-    setIsFocused(true)
-  }
+  // useEffect(() => {
+  //   console.log("useEffect happened", route);
+  //   // navigate.push('/')
+    
+  // }, [route])
 
-  const handleBlur = () => {
-    setIsFocused(false)
-  }
-
-  // useEffect(() =>{
-  // setInitialParams(navigate.query)
-  // console.log("USEEFFECT STATE: ", initialParams);
-  // }, [initialParams])
-
-  // console.log("INITIAL PARAMS STATE: ", initialParams);
-  
   return (
     <div className='relative'>
       <form onSubmit={handleSubmit}>
@@ -112,8 +84,9 @@ const SearchInput = () => {
           className='flex h-10 pr-14 pl-10 text-sm text-cyan-400 bg-transparent rounded-md border border-cyan-400 focus:outline-none focus:border-1 focus:border-solid focus:border-cyan-300 hover:neon-shadow-soft duration-200'
           type='text'
           placeholder='Search Movie'
-          onFocus={handleFocus}
-          onBlur={handleBlur}
+          // onFocus={handleFocus}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
           value={inputVal}
           onChange={handleChange}
           onKeyDown={(e) => e.stopPropagation()}
