9256aa0b1ede14d961dca42d572092c03589166f
Lisa Wagner <lisawagner.online@gmail.com>
2022-09-25 16:59:06 -0700

    styles: update slider button border

--- components/Carousel/CarouselButton.tsx
+++ components/Carousel/CarouselButton.tsx
@@ -13,12 +13,13 @@ const CarouselButton = ({ onClick, isPrev }: TProps) => {
   return (
 
     <button
-      className={`absolute z-10 top-1/2 -translate-y-1/2 w-10 h-24 border-spacing-1 text-cyan-400 hover:text-cyan-300 border-2 border-cyan-400 bg-brand-900 hover:neon-shadow-soft ${isPrev ? 'left-0' : 'right-0 '}`}
-      onClick={onClick}>
-        <div className='flex items-center justify-center text-7xl'>
-          {isPrev ? <RiArrowLeftSLine /> : <RiArrowRightSLine />}
-        </div>
-      </button>
+      className={`absolute z-10 top-1/2 -translate-y-1/2 w-10 h-24 border-spacing-1 text-cyan-500 hover:text-cyan-300 border border-y-cyan-400 bg-brand-900 hover:neon-shadow-soft duration-200 ${isPrev ? 'left-0 rounded-r-lg border-r-cyan-500 border-l-0' : 'right-0 rounded-l-lg border-l-cyan-500 border-r-0'}`}
+      onClick={onClick}
+    >
+      <div className='flex items-center justify-center text-7xl'>
+        {isPrev ? <RiArrowLeftSLine /> : <RiArrowRightSLine />}
+      </div>
+    </button>
   )
 }
 export default CarouselButton
