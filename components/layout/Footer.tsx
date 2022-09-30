bc9f30cea524a9730826e7a60c6251c67e1213c8
Lisa Wagner <lisawagner.online@gmail.com>
2022-09-30 12:01:53 -0700

    fix: add 'noreferrer' to external links

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,10 +5,10 @@ const Footer = () => {
 
   return (
     <div className="relative w-full flex items-center justify-between text-cyan-500 font-bold mb-24 px-20 pt-6 bg-brand-900">
-      <a className='text-4xl' href='https://github.com/lisawagner' target="_blank">
+      <a className='text-4xl' href='https://github.com/lisawagner' target="_blank" rel="noreferrer">
         <RiGithubFill />
       </a>
-      <a href='https://www.themoviedb.org/' target="_blank">
+      <a href='https://www.themoviedb.org/' target="_blank" rel="noreferrer">
         <Image width='100' height='20' src='/images/tmdb.svg' alt='tmdb logo' />
       </a>
     </div>
