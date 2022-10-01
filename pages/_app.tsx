a03a82a56e0e444e03ea6416e7dadccfcf2644b2
Lisa Wagner <lisawagner.online@gmail.com>
2022-10-01 09:51:08 -0700

    feat: add basic meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
+import Head from 'next/head';
 import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 // components
-// import { Layout } from '../components'
 import { Layout } from '../components'
 // next imports
 import type { AppProps } from 'next/app'
@@ -13,6 +13,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+      <Head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#010404" />
+        <meta
+          name='description'
+          content='filmClu is a movie search app created with NextJS, tailwind and typescript'/>
+        <link rel="apple-touch-icon" href="/logo192.png" /> 
+        <link rel="manifest" href="/manifest.json" />
+
+        <title>filmClu 2.0 | Amazing Movies to Discover</title>
+      </Head>
         <Layout>
           <Component {...pageProps} />
         </Layout>
