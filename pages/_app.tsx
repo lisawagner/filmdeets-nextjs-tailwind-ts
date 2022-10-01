9a75b7b94d067d29ff2da0c55fabfeb1ef3c4a4f
Lisa Wagner <lisawagner.online@gmail.com>
2022-10-01 10:19:38 -0700

    fix: swap metadata entry points

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 // components
@@ -12,6 +13,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+      <Head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#010404" />
+        <meta
+          name='description'
+          content='filmClu is a movie search app created with NextJS, tailwind and typescript'/>
+        <link rel="apple-touch-icon" href="/logo192.png" /> 
+        <link rel="manifest" href="/manifest.json" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel='stylesheet' />
+
+        <title>filmClu 2.0 | Amazing Movies to Discover</title>
+      </Head>
         <Layout>
           <Component {...pageProps} />
         </Layout>
